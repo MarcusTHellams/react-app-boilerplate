a5ce8559237f36e5fbd5597e97cccbbb205cc458
Marcus Hellams <marcus.hellams@accenturefederal.com>
2016-06-14 16:02:30 -0400

    Props.Children

--- app/App.js
+++ app/App.js
@@ -15,9 +15,9 @@ class GroceryList extends Component {
   render(){
     return (
       <ul>
-      <ListItem quantity="1" name="Bread" />
-      <ListItem quantity="6" name="Eggs" />
-      <ListItem quantity="2" name="Milk" />
+      <ListItem quantity="1"> Bread</ListItem>
+      <ListItem quantity="6"> Eggs</ListItem>
+      <ListItem quantity="2"> Milk</ListItem>
       </ul>
     );
   }
@@ -27,7 +27,7 @@ class ListItem extends Component {
   render(){
     return (
      <li>
-		{this.props.quantity} x {this.props.name}
+		{this.props.quantity} x {this.props.children}
      </li>
     );
   }
