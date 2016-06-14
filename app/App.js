199a7033464d1b98c35c31287a7511c035a78d1e
Marcus Hellams <marcus.hellams@accenturefederal.com>
2016-06-14 15:59:04 -0400

    Grocery List

--- app/App.js
+++ app/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'; 
 import {render} from 'react-dom';
 
 class Hello extends Component {
@@ -10,4 +10,28 @@ class Hello extends Component {
   }
 }
 
-render(<Hello />, document.getElementById('root'));
+
+class GroceryList extends Component {
+  render(){
+    return (
+      <ul>
+      <ListItem quantity="1" name="Bread" />
+      <ListItem quantity="6" name="Eggs" />
+      <ListItem quantity="2" name="Milk" />
+      </ul>
+    );
+  }
+}
+
+class ListItem extends Component {
+  render(){
+    return (
+     <li>
+		{this.props.quantity} x {this.props.name}
+     </li>
+    );
+  }
+}
+
+
+render(<GroceryList />, document.getElementById('root'));
